0c7a806b86bc47a0feb5b1cf0312f579aa6c095a
Taylor <taylor.steinbaugh@gmail.com>
2025-12-21 18:19:27 -0700

    broke up prisma schema file to many, small files

--- backend/prisma.config.mjs
+++ backend/prisma.config.mjs
@@ -1,8 +1,9 @@
+// backend/prisma.config.mjs
 import "dotenv/config";
 import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
+  schema: "prisma",
   migrations: { path: "prisma/migrations" },
   engine: "classic",
   datasource: { url: env("DATABASE_URL") },
---
+++ backend/prisma/migrations/20251222011802_update_file_structure/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `isPrimary` on the `LeaseTenant` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "LeaseTenant" DROP COLUMN "isPrimary";
